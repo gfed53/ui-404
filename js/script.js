3aadc41e4cd0357c0188494ba7fb2c9955892777
gfed53 <gfederico7153@gmail.com>
2017-10-01 17:49:15 -0400

    Added animation for header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 $(function(){
 	console.log('Hello world');
+
+	animateHeader();
+
+	function animateHeader(){
+		$('#h-404')
+		.velocity({
+			opacity: 1,
+			scaleX: 1.5
+		},{
+			duration: 500
+		})
+		.velocity({
+			scaleY: 1.5
+		},{
+			duration: 500
+		});
+	}
 });

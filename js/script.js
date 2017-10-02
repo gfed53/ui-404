844e781bf08a910391eda5392b54f40d5f101df9
gfed53 <gfederico7153@gmail.com>
2017-10-02 13:54:15 -0400

    Added animation cycles for header

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
 $(function(){
 	console.log('Hello world');
 
+	setInterval(animateHeader, 2000);
+
 	animateHeader();
 
 	function animateHeader(){
+		let base = 10;
+		let choices = [600,750,900];
+		let points = [];
+
+		for(let i = 0; i < 4; i++){
+			points[i] = choices[Math.floor(Math.random() * choices.length)];
+		}
+
+		console.log(points);
+
 		$('#h-404')
 		.velocity({
-			opacity: 1,
 			scaleX: 1.5
 		},{
-			duration: 500
+			duration: points[0]
 		})
 		.velocity({
 			scaleY: 1.5
 		},{
-			duration: 500
+			duration: points[1]
+		})
+		.velocity({
+			scaleY: 0.75
+		},{
+			duration: points[2]
+		})
+		.velocity({
+			scaleX: 0.75
+		},{
+			duration: points[3]
 		});
 	}
 });
